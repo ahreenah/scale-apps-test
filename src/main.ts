8ad7be172939b454acc0451269aa8de3c9b5e841
Arina Melianova <ah.ree.nah@gmail.com>
2025-07-17 21:38:17 -0300

    Make responsive page

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+
+const menuToggle: HTMLInputElement | null = document.querySelector("#menuToggle")
+const headerLinks = document.querySelectorAll('header a')
+
+if(menuToggle){
+  for(const link of headerLinks){
+    link.addEventListener('click', ()=>menuToggle.checked = false)
+  }
+}
